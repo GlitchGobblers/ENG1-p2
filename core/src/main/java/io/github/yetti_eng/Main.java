28853b21420ca0e389adc363c0666b9483b2aea8
KatelynsGit <234409954+KatelynsGit@users.noreply.github.com>
2025-11-04 14:32:59 +0000

    Implement Timer class

--- core/src/main/java/io/github/yetti_eng/Main.java
+++ core/src/main/java/io/github/yetti_eng/Main.java
@@ -2,6 +2,7 @@ package io.github.yetti_eng;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -17,6 +18,8 @@ import io.github.yetti_eng.entities.Wall;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+    private static final int TIMER_LENGTH = 300; // 300s = 5min
+
     private SpriteBatch batch;
     private FitViewport viewport;
 
@@ -29,6 +32,8 @@ public class Main extends ApplicationAdapter {
     private FreeTypeFontGenerator robotoGenerator;
     private final FreeTypeFontParameter fontParameter = new FreeTypeFontParameter();
     private BitmapFont roboto32;
+
+    private Timer timer;
     private Label timerText;
 
     @Override
@@ -45,6 +50,9 @@ public class Main extends ApplicationAdapter {
         robotoGenerator = new FreeTypeFontGenerator(Gdx.files.internal("Roboto-VariableFont_wdth,wght.ttf"));
         fontParameter.size = 32;
         roboto32 = robotoGenerator.generateFont(fontParameter);
+
+        timer = new Timer(TIMER_LENGTH);
+        timer.play(); // TODO: move this to after leaving the menu screen
         timerText = new Label(null, new Label.LabelStyle(roboto32, Color.BLACK.cpy()));
         timerText.setPosition(0, 10);
     }
@@ -97,6 +105,29 @@ public class Main extends ApplicationAdapter {
 
         player.setX(MathUtils.clamp(player.getX(), 0, worldWidth - playerWidth));
         player.setY(MathUtils.clamp(player.getY(), 0, worldHeight - playerHeight));
+
+        // Timer
+        // For testing purposes
+        if (!timer.isActive()) {
+            timerText.setStyle(new Label.LabelStyle(roboto32, Color.RED.cpy()));
+        } else {
+            timerText.setStyle(new Label.LabelStyle(roboto32, Color.BLACK.cpy()));
+        }
+
+        if (timer.hasElapsed()) {
+            timer.finish();
+            System.out.println("Timer elapsed!");
+            timer.finish();
+        }
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            if (timer.isActive()) {
+                timer.pause();
+            } else {
+                timer.play();
+            }
+        }
+        // For testing purposes END
     }
 
     private void draw() {
@@ -109,10 +140,8 @@ public class Main extends ApplicationAdapter {
         player.draw(batch);
         testWall.draw(batch);
 
-        // TODO
-        // int timeRemaining = getCurrentTimeRemaining();
-        // String text = (timeRemaining / 60) + ":" + String.format("%02d", timeRemaining % 60);
-        String text = "TEST";
+        int timeRemaining = timer.getRemainingTime();
+        String text = (timeRemaining / 60) + ":" + String.format("%02d", timeRemaining % 60);
         timerText.setText(text);
         timerText.draw(batch, 1.0f);
 
