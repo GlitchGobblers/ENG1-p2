9cedb0f91469749d9d83aa2798a01d291a1e0754
BenSlater28 <160119942+BenSlater28@users.noreply.github.com>
2025-12-03 15:32:25 +0000

    Started to get the events to work

--- core/src/main/java/io/github/yetti_eng/events/Event.java
+++ core/src/main/java/io/github/yetti_eng/events/Event.java
@@ -6,11 +6,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.math.Vector2;
 import io.github.yetti_eng.YettiGame;
+import io.github.yetti_eng.entities.Entity;
 import io.github.yetti_eng.entities.Item;
 import io.github.yetti_eng.entities.Player;
 import io.github.yetti_eng.screens.GameScreen;
 
-public class Event {
+public class Event extends Entity {
     private int counter = 0;
     private String interactionMessage;
     private String interactionImagePath;
@@ -21,6 +22,12 @@ public class Event {
     private boolean visible;
     private SpriteBatch batch;
     public Event(MapProperties properties){
+        super(new Texture((String) properties.get("interactionImage")),
+            (Float) properties.get("x"),
+            (Float) properties.get("y"),
+            (Float) properties.get("width"),
+            (Float) properties.get("height"),
+            0, true);
         interactionMessage = (String) properties.get("interactionMessage");
         interactionImagePath = (String) properties.get("interactionImage");
         interactionImage = new Texture(interactionImagePath);
@@ -28,6 +35,7 @@ public class Event {
         interactionSize = new Vector2((Float) properties.get("width"), (Float) properties.get("height"));
         scoreModifier = (Integer) properties.get("scoreModifier");
         visible = (Boolean) properties.get("visible");
+
     }
     public int getScoreModifier(){
         return scoreModifier;
