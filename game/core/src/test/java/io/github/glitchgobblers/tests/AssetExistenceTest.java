f866aabe8f5491351abe5c26b501433e894a94f3
dash <gcx519@york.ac.uk>
2025-12-21 11:35:52 +0000

    update assets list

--- game/core/src/test/java/io/github/glitchgobblers/tests/AssetExistenceTest.java
+++ game/core/src/test/java/io/github/glitchgobblers/tests/AssetExistenceTest.java
@@ -12,13 +12,45 @@ public class AssetExistenceTest extends GdxTestBase {
         // List of critical assets referenced in GameScreen.java
         // THIS MAY NEED TO BE UPDATED AS PROJECT DEVELOPS
         String[] requiredAssets = {
+            // Character sprites
             "character/player_up.png",
             "character/player_down.png",
+            "character/player_left.png",
+            "character/player_right.png",
             "character/yeti.png",
+
+            // Items
+            "item/exit.png",
             "item/checkin_code.png",
             "item/door.png",
+            "item/doorframe.png",
+            "item/long_boi.png",
+            "item/water_spill.png",
+
+            // Map
             "map/map.tmx",
+
+            // UI textures
+            "ui/pause.png",
+            "ui/slider_background.png",
+            "ui/slider_knob.png",
+
+            // Achievement icons
+            "ui/ach_welcome.png",
+            "ui/ach_pause.png",
+            "ui/ach_dean.png",
+            "ui/ach_timeout.png",
+            "ui/ach_event.png",
+            "ui/ach_completionist.png",
+
+            // Audio
             "audio/duck_quack.mp3",
+            "audio/paper_rustle.wav",
+            "audio/dorm_door_opening.wav",
+            "audio/cartoon_quick_slip.wav",
+            "audio/deep_growl_1.wav",
+
+            // Fonts
             "Roboto.ttf"
         };
 
